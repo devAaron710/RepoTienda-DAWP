8297eee95f176d8d5ca80d443cf07356cc0bad68
Aaron Azofeifa <aazofeifa60571@ufide.ac.cr>
2025-06-04 23:53:49 -0600

    Finalización Sem4

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller /*@ ANOTACIONES O DECORADORES*/
+public class IndexController {
+    
+    @RequestMapping("/")//url al que responde
+    public String index(Model model) {
+        return "index";//nombre de la vista en templates a mostrar
+    }
+    
+    @RequestMapping("/informacion")
+    public String info() {
+        return "info";
+    }
+    
+}
