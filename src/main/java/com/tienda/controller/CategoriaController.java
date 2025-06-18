ea31faf77d034418e865a75e92863936fbf40e5a
Aaron Azofeifa <aazofeifa60571@ufide.ac.cr>
2025-06-17 21:09:16 -0600

    Finalización Sem6

--- src/main/java/com/tienda/controller/CategoriaController.java
+++ src/main/java/com/tienda/controller/CategoriaController.java
@@ -3,27 +3,67 @@ package com.tienda.controller;
 
 import com.tienda.domain.Categoria;
 import com.tienda.service.CategoriaService;
-import java.util.List;
+import com.tienda.service.impl.FirebaseStorageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/categoria")
 public class CategoriaController {
     
-    @Autowired
-    CategoriaService categoriaService;
-    
+   @Autowired
+    private CategoriaService categoriaService;
+   @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
+
     @GetMapping("/listado")
-    public String listado(Model model) {
-        List<Categoria> categorias = categoriaService.geCategorias(false);
+    public String inicio(Model model) {
+        var categorias = categoriaService.getCategorias(false);
         model.addAttribute("categorias", categorias);
         model.addAttribute("totalCategorias", categorias.size());
-        
         return "/categoria/listado";
     }
     
+    @GetMapping("/nuevo")
+    public String categoriaNuevo(Categoria categoria) {
+        return "/categoria/modifica";
+    }
+    
+    @PostMapping("/guardar")
+    public String categoriaGuardar(Categoria categoria,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {  
+        //verificamos que la imagen sea distinto de vacio
+        if (!imagenFile.isEmpty()) {
+            //guardar registro de la categoria
+            categoriaService.save(categoria);
+            categoria.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile, 
+                            "categoria", 
+                            categoria.getIdCategoria()));
+        }
+        categoriaService.save(categoria);
+        //redirect vaya a una accion con este nombre que me sirve para hacer algo previo
+        return "redirect:/categoria/listado";
+    }
+
+    //rediccionamiento a un url
+    @GetMapping("/eliminar/{idCategoria}")
+    public String categoriaEliminar(Categoria categoria) {
+        categoriaService.delete(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/modificar/{idCategoria}")
+    public String categoriaModificar(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        return "/categoria/modifica";
+    }
 }
--- src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
+++ src/main/java/com/tienda/service/impl/CategoriaServiceImpl.java
@@ -7,25 +7,41 @@ import com.tienda.service.CategoriaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 //define la logica de los metodos de la interfaz que implementa
 @Service
 public class CategoriaServiceImpl implements CategoriaService{
     
-    //inyeccion
     @Autowired
     private CategoriaDao categoriaDao;
 
     @Override
-    public List<Categoria> geCategorias(boolean activos) {
-        List<Categoria> lista = categoriaDao.findAll();
-        
-        //filtrar por activos
-        if (activos){
-            lista.removeIf(cat -> !cat.isActivo());
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaDao.findAll();
+        if (activos) {
+            lista.removeIf(e -> !e.isActivo());
         }
-        
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.deleteById(categoria.getIdCategoria());
     }
     
 }
